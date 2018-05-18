febe86c6a1fdb5c1f2d95464f9264f92395c2bd9
Maruti <marutitathe@gmail.com>
2018-05-18 19:33:07 +0530

    t

--- www/js/main.js
+++ www/js/main.js
@@ -16,3 +16,20 @@ function goPage(pageName) {
     alert(pageName)
     window.location.href = pageName;
 }
+
+function logStep(str,isNew)
+{
+    if (isNew == 1)
+    {
+        localStorage.clear();
+        localStorage.setItem("logStep", "DateTime : " + getCurrentDateTime()) + "... \r\n";
+    }
+    str = localStorage.getItem("logStep") + "... \r\n" + str;
+    localStorage.setItem("logStep", str);
+}
+
+function getCurrentDateTime()
+{
+    d = new Date();
+    return d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMilliseconds();
+}
